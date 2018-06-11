6bcd91f6b32fbf8a4d317f68740a7ee019530ff8
TAFE\6105411017 <christiancouper@gmail.com>
2018-06-12 09:22:51 +1000

    Creation of Get methods

--- EITS_ESC/src/DB_model.java
+++ EITS_ESC/src/DB_model.java
@@ -1,6 +1,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,6 +24,31 @@ public class DB_model {
         } catch( Exception e) {
           e.printStackTrace();
           return null;
+          
         } // end of catch
-    } // end of get
+    } // end of get connection
+    public ArrayList<Admin> getAdminList(){
+         
+    }//end of getAdminList:christian
+     
+    public ArrayList<Caseworkers> getCaseworkersList(){
+         
+    }//end of getCaseworkers:christian
+     
+    public ArrayList<Courses> getCoursesList(){
+         
+    }//end of getCoursesList:christian
+    public ArrayList<Customers> getCustomersList(){
+         
+    }//end of getCustomersList:christian
+    public ArrayList<Units> getUnitsList(){
+         
+    }//end of getUnitsList:christian
+    public ArrayList<Units> getUnitsList(){
+         
+    }//end of getUnitsList:christian
+    
+    //END OF GETS:christian
+    
+    
 }
