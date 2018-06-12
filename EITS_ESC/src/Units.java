a689ac4600bd66f43afce1d70b777f8c2e16ead1
TAFE\6105411017 <christiancouper@gmail.com>
2018-06-12 11:22:40 +1000

    Sets and Gets for Units Class

--- EITS_ESC/src/Units.java
+++ EITS_ESC/src/Units.java
@@ -9,5 +9,34 @@
  * @author 6105411017
  */
 public class Units {
-    
-}
+    private String units_Id;
+    private String TitleUnits;
+
+    /**
+     * @return the units_Id
+     */
+    public String getUnits_Id() {
+        return units_Id;
+    }
+
+    /**
+     * @param units_Id the units_Id to set
+     */
+    public void setUnits_Id(String units_Id) {
+        this.units_Id = units_Id;
+    }
+
+    /**
+     * @return the TitleUnits
+     */
+    public String getTitleUnits() {
+        return TitleUnits;
+    }
+
+    /**
+     * @param TitleUnits the TitleUnits to set
+     */
+    public void setTitleUnits(String TitleUnits) {
+        this.TitleUnits = TitleUnits;
+    }
+}//End of Units Class sets and gets: Christian
