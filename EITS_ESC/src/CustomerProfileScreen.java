6d7135201adaff5b58963654910e5964194137fa
ALEXIS-PASALIC\user <pasalic_alexis@gmail.com>
2018-06-25 11:16:18 +1000

    correct the banners

--- EITS_ESC/src/CustomerProfileScreen.java
+++ EITS_ESC/src/CustomerProfileScreen.java
@@ -41,7 +41,7 @@ public class CustomerProfileScreen extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.setBackground(new java.awt.Color(171, 233, 209));
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jPanel1.setPreferredSize(new java.awt.Dimension(530, 93));
 
